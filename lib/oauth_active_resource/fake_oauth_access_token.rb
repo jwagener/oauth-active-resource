f3f77732205b14c673c034dcfe363e344d55bd4f
jwagener <johannes@wagener.cc>
2009-05-20 18:48:47 +0200

    added fake oauth, uniqeu resource array

---
+++ lib/oauth_active_resource/fake_oauth_access_token.rb
@@ -0,0 +1,55 @@
+module OAuthActiveResource
+  
+  # just simulates the request and sign! methods of a oauth access token
+  class FakeOAuthAccessToken < OAuth::Consumer
+    
+    attr_accessor :token, :secret
+    def initialize()
+      @key    = nil
+      token = 'Anonymous'
+      secret = 'Anonymous'
+      
+      # ensure that keys are symbols
+      @options = @@default_options    
+    end
+    
+    def request(http_method, path, token = nil, request_options = {}, *arguments)
+      if path !~ /^\//
+        @http = create_http(path)
+        _uri = URI.parse(path)
+        path = "#{_uri.path}#{_uri.query ? "?#{_uri.query}" : ""}"
+
+      end
+
+      rsp = http.request(create_http_request(http_method, path, token, request_options, *arguments))
+
+      rsp
+    end
+    
+    def get(path, headers = {})
+      request(:get, path, headers)
+    end
+ 
+    def head(path, headers = {})
+      request(:head, path, headers)
+    end
+ 
+    def post(path, body = '', headers = {})
+      request(:post, path, body, headers)
+    end
+
+    def put(path, body = '', headers = {})
+      request(:put, path, body, headers)
+    end
+
+    def delete(path, headers = {})
+      request(:delete, path, headers)
+    end
+    
+    def sign!(*args)
+      # do nothing
+    end
+    
+  end
+end
+
---
+++ lib/oauth_active_resource/unique_resource_array.rb
@@ -0,0 +1,86 @@
+require 'set'
+
+module OAuthActiveResource
+  class UniqueArray < Array
+    def initialize(*args)
+      if args.size == 1 and args[0].is_a? Array then
+        super(args[0].uniq)
+      else
+        super(*args)
+      end
+    end
+
+    def insert(i, v)
+      super(i, v) unless include?(v)
+    end
+
+    def <<(v)
+      super(v) unless include?(v)
+    end
+
+    def []=(*args)
+      # note: could just call super(*args) then uniq!, but this is faster
+
+      # there are three different versions of this call:
+      # 1. start, length, value
+      # 2. index, value
+      # 3. range, value
+      # We just need to get the value
+      v = case args.size
+        when 3 then args[2]
+        when 2 then args[1]
+        else nil
+      end
+
+      super(*args) if v.nil? or not include?(v)
+    end
+  end
+  
+  # see has_many in Resource
+  class UniqueResourceArray < UniqueArray
+    def initialize(connection, resource,  collection_path)   
+      super()   
+      
+      @connection = connection
+      @collection_path = collection_path
+      @resource = resource
+      reload
+    end
+   
+    def to_json
+      return "[ #{self.map { |obj| obj.to_json }.join(',')} ]"
+    end
+    
+    def to_xml
+      # or use __method__ here?
+      pt = @resource.element_name.pluralize
+      return "<#{pt}> #{self.map { |obj| obj.to_xml({:skip_instruct => true})}.join(' ')} </#{pt}>"
+    end
+    
+    
+    # DEPRECATED...
+    #def find(look_for)
+    #  if not (look_for.is_a? String or look_for.is_a? Integer)
+    #    look_for_id = look_for
+    #  else      
+    #    look_for_id = look_for.id
+    #  end  
+    #  
+    #  self.each do |obj|
+    #      obj.id == look_for_id and return obj 
+    #  end
+    #  return nil
+    #end
+    
+    def save
+      response = @connection.handle_response( @connection.put("#{@collection_path}",self.to_xml) )
+      self.replace( @resource.load_collection( @connection.format.decode(response.body) ) )
+    end
+    
+    def reload
+      self.replace(@resource.find(:all, :from => @collection_path))
+      return self
+    end
+  end
+
+end
